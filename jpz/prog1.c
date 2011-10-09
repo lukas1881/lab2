6642372c6c9acb8b4d21ec058175d64c38a31432
Lukas <lsady@inf.ug.edu.pl>
2011-10-09 13:12:24 +0200

    dodanie prog1.c

---
+++ jpz/prog1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+float n1,n2,n3;
+int main() {
+  // n1=5+3+8/2-3;
+  //n2=17%3*11;
+  //n3=2*4*(5+9.8/2);
+  printf ("Podaj trzy liczby\n");
+    scanf ("%f", &n1);
+    scanf ("%f", &n2);
+    scanf ("%f", &n3);
+  printf("n1 = %f\nn2 = %f\nn3 = %f\n\n", n1,n2,n3);
+  //return 0;
+}
